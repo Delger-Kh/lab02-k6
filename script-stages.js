a2f2333c747abe359218f053f493daf0b4e76670
Delger-Kh <delgerdelger107@gmail.com>
2026-09-17 10:55:33 +0800

    Add stages script demonstrating ramped load 5->30->100->0 VU

---
+++ script-stages.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '20s', target: 5 },
+    { duration: '20s', target: 30 },
+    { duration: '20s', target: 100 },
+    { duration: '20s', target: 0 },
+  ],
+};
+
+export default function () {
+  const res = http.get('https://test.k6.io');
+  check(res, { 'status is 200': (r) => r.status === 200 });
+  sleep(1);
+}
